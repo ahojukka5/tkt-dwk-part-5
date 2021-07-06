877ac790c411cfb5f560076ae6e0b6bde6bac4d5
Jukka Aho <ahojukka5@gmail.com>
2021-07-07 00:29:19 +0300

    Add tags to 4.01

--- build_images.sh
+++ build_images.sh
@@ -2,20 +2,11 @@
 
 set -e
 
-docker build -t ahojukka5/dwk-mainapp-gen-timestamp mainapp/gen-timestamp
-docker push ahojukka5/dwk-mainapp-gen-timestamp
+docker build -t ahojukka5/dwk-mainapp-gen-timestamp:4.01 mainapp/gen-timestamp && docker push ahojukka5/dwk-mainapp-gen-timestamp:4.01
+docker build -t ahojukka5/dwk-mainapp-read-timestamp:4.01 mainapp/read-timestamp && docker push ahojukka5/dwk-mainapp-read-timestamp:4.01
+docker build -t ahojukka5/dwk-pingpong:4.01 pingpong && docker push ahojukka5/dwk-pingpong:4.01
 
-docker build -t ahojukka5/dwk-mainapp-read-timestamp mainapp/read-timestamp
-docker push ahojukka5/dwk-mainapp-read-timestamp
-
-docker build -t ahojukka5/dwk-pingpong pingpong && docker push ahojukka5/dwk-pingpong
-
-docker build -t ahojukka5/dwk-todo-backend todo-backend && docker push ahojukka5/dwk-todo-backend
-
-docker build -t ahojukka5/dwk-todo-frontend todo-frontend
-docker push ahojukka5/dwk-todo-frontend
-
-docker build -t ahojukka5/dwk-todo-cronjob todo-cronjob && docker push ahojukka5/dwk-todo-cronjob
-
-docker build -t ahojukka5/dwk-secrets secrets
-docker push ahojukka5/dwk-secrets
+# docker build -t ahojukka5/dwk-todo-backend todo-backend && docker push ahojukka5/dwk-todo-backend
+# docker build -t ahojukka5/dwk-todo-frontend todo-frontend && docker push ahojukka5/dwk-todo-frontend
+# docker build -t ahojukka5/dwk-todo-cronjob todo-cronjob && docker push ahojukka5/dwk-todo-cronjob
+# docker build -t ahojukka5/dwk-secrets secrets && docker push ahojukka5/dwk-secrets

b60d14bf42475d003ebf8d527d7b0b63862a8d0e
Jukka Aho <ahojukka5@gmail.com>
2021-07-07 22:46:10 +0300

    Update build_images.sh

--- build_images.sh
+++ build_images.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-docker build -t ahojukka5/dwk-mainapp-gen-timestamp:4.01 mainapp/gen-timestamp && docker push ahojukka5/dwk-mainapp-gen-timestamp:4.01
-docker build -t ahojukka5/dwk-mainapp-read-timestamp:4.01 mainapp/read-timestamp && docker push ahojukka5/dwk-mainapp-read-timestamp:4.01
-docker build -t ahojukka5/dwk-pingpong:4.01 pingpong && docker push ahojukka5/dwk-pingpong:4.01
+export TAG=4.02
 
-# docker build -t ahojukka5/dwk-todo-backend todo-backend && docker push ahojukka5/dwk-todo-backend
-# docker build -t ahojukka5/dwk-todo-frontend todo-frontend && docker push ahojukka5/dwk-todo-frontend
-# docker build -t ahojukka5/dwk-todo-cronjob todo-cronjob && docker push ahojukka5/dwk-todo-cronjob
-# docker build -t ahojukka5/dwk-secrets secrets && docker push ahojukka5/dwk-secrets
+docker build -t ahojukka5/dwk-mainapp-gen-timestamp:$TAG mainapp/gen-timestamp && docker push ahojukka5/dwk-mainapp-gen-timestamp:$TAG
+docker build -t ahojukka5/dwk-mainapp-read-timestamp:$TAG mainapp/read-timestamp && docker push ahojukka5/dwk-mainapp-read-timestamp:$TAG
+docker build -t ahojukka5/dwk-pingpong:$TAG pingpong && docker push ahojukka5/dwk-pingpong:$TAG
+docker build -t ahojukka5/dwk-todo-backend:$TAG todo-backend && docker push ahojukka5/dwk-todo-backend:$TAG
+docker build -t ahojukka5/dwk-todo-frontend:$TAG todo-frontend && docker push ahojukka5/dwk-todo-frontend:$TAG
+docker build -t ahojukka5/dwk-todo-cronjob:$TAG todo-cronjob && docker push ahojukka5/dwk-todo-cronjob:$TAG

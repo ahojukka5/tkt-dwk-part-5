cd9ee1ae2de8973cf8b2d5579936d463677bf7e5
Jukka Aho <ahojukka5@gmail.com>
2021-07-10 22:27:02 +0300

    Bump version to 5.05

--- build_images.sh
+++ build_images.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export TAG=4.08
+export TAG=5.05
 
 docker build -t ahojukka5/dwk-mainapp-gen-timestamp:$TAG mainapp/gen-timestamp && docker push ahojukka5/dwk-mainapp-gen-timestamp:$TAG
 docker build -t ahojukka5/dwk-mainapp-read-timestamp:$TAG mainapp/read-timestamp && docker push ahojukka5/dwk-mainapp-read-timestamp:$TAG
